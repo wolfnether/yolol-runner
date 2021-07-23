f172dac9288ac3446a4da3e00553dc2d3c00ebcb
alrianne <alrianne>
2021-07-23 18:30:11 +0200

    clippy pass

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use walkdir::WalkDir;
 use yolol_devices::devices::chip::CodeRunner;
 use yolol_runner::YololRunner;
 
-use core_affinity;
-
 fn main() -> Result<(), Box<dyn Error>> {
     let core_ids = core_affinity::get_core_ids().unwrap();
     core_affinity::set_for_current(core_ids[0]);
@@ -15,16 +13,25 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for entry in WalkDir::new(env::current_dir()?) {
         let entry = entry?;
-        if entry.file_name().to_str().map(|s| s.ends_with(".yolol")).unwrap_or(false){
-            let rel_path = entry.path().strip_prefix(env::current_dir()?)?.to_str().unwrap();
+        if entry
+            .file_name()
+            .to_str()
+            .map(|s| s.ends_with(".yolol"))
+            .unwrap_or(false)
+        {
+            let rel_path = entry
+                .path()
+                .strip_prefix(env::current_dir()?)?
+                .to_str()
+                .unwrap();
             println!("## {}", rel_path);
             let timer = SystemTime::now();
             runner.parse(rel_path);
             println!(" - Compiled:\t {} ms", timer.elapsed()?.as_millis());
-            
+
             let timer = SystemTime::now();
 
-            let mut iteration:usize = 1_000_000;
+            let mut iteration: usize = 1_000_000;
             let mut samples = vec![];
             let mut total_line_count = 0;
 
@@ -35,18 +42,18 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
                 total_line_count += iteration;
                 let lps = iteration as f64 / started.elapsed().unwrap().as_secs_f64();
-                iteration =lps as usize / 10;
+                iteration = lps as usize / 10;
                 samples.push(lps);
                 let len = samples.len() as f64;
                 let iter = samples.iter().rev().take(10);
                 let avg = iter.clone().sum::<f64>() / iter.len() as f64;
                 let sum = samples.iter().map(|i| (*i - avg).powi(2)).sum::<f64>();
                 let dev = (sum / (len - 1.)).sqrt();
-        
+
                 println!(" - Total:\t {} lines", total_line_count);
                 println!(" - Average:\t {:.2} l/s", avg);
                 println!(" - StdDev:\t {:.2}", dev);
-        
+
                 if let Some(output) = runner.get_global().iter().find(|f| f.name() == "output") {
                     if **output != "ok".into() {
                         println!(
