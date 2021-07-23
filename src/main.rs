5afe3ad11e6817fb954a272e0e3a85ab644e0326
alrianne <alrianne>
2021-07-23 17:30:27 +0200

    try to impletment reference mplementation benchmark

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,39 @@ use yolol_devices::devices::chip::CodeRunner;
 use yolol_runner::YololRunner;
 
 fn main() -> Result<(), SystemTimeError> {
-    const LINES_PER_ITERATION: usize = 100_000_000;
+    let mut iteration:usize = 1_000_000;
     let mut samples = vec![];
     let mut runner = YololRunner::default();
+    let mut total_line_count = 0;
     runner.parse("speedrun.yolol");
     //panic!("{:?}", runner);
     loop {
         let started = SystemTime::now();
-        for _ in 0..LINES_PER_ITERATION {
+        for _ in 0..iteration {
             runner.step();
         }
-        let lps = LINES_PER_ITERATION as f64 / started.elapsed().unwrap().as_secs_f64();
+        total_line_count += iteration;
+        let lps = iteration as f64 / started.elapsed().unwrap().as_secs_f64();
+        iteration =lps as usize / 10;
         samples.push(lps);
         let len = samples.len() as f64;
-        let avg = samples.iter().sum::<f64>() / len;
+        let iter = samples.iter().rev().take(10);
+        let avg = iter.clone().sum::<f64>() / iter.len() as f64;
         let sum = samples.iter().map(|i| (*i - avg).powi(2)).sum::<f64>();
         let dev = (sum / (len - 1.)).sqrt();
 
-        println!(" - Total:\t {} lines", LINES_PER_ITERATION * samples.len());
+        println!(" - Total:\t {} lines", total_line_count);
         println!(" - Average:\t {:.2} l/s", avg);
         println!(" - StdDev:\t {:.2}", dev);
+
+        if let Some(output) = runner.get_global().iter().find(|f| f.name() == "output") {
+            if **output != "ok".into() {
+                println!(
+                    " - FAILED! Expected `:OUTPUT==\"ok\"`, got ``:OUTPUT==\"{}\"``",
+                    **output
+                );
+            }
+        }
 
         //println!("{:?}", runner.get_global().iter().find(|i| i.name() == "output"))
     }
